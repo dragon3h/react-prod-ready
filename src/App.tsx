bb5888189d23983ad4ffaf6ffe251fe9faa42a09
dragon3h <dragon3h@gmx.com>
2023-07-23 14:22:13 -0400

    feat: added Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import "./index.scss";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
+import { Suspense, useState } from "react";
 import { Route, Routes, Link } from "react-router-dom";
+
+import "./styles/index.scss";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
 import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
-import { Suspense } from "react";
+
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
 
 export const App = () => {
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === Theme.LIGHT ? Theme.DARK : Theme.LIGHT));
+  };
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+
       <Link to={"/"}>Main Page</Link>
       <Link to={"/about"}>About Page</Link>
+
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path={"/about"} element={<AboutPageAsync />} />
